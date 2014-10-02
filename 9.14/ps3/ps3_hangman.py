12037303fee04c2da349809f0675d4fba1a116d0
dixoncox <dixonbush@yahoo.com>
2014-10-01 18:30:41 -0700

    start of ps4

--- 9.14/ps3/ps3_hangman.py
+++ 9.14/ps3/ps3_hangman.py
@@ -149,6 +149,6 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-#secretWord = chooseWord(wordlist).lower()
-secretWord = 'zzz'
+secretWord = chooseWord(wordlist).lower()
+#secretWord = 'zzz'
 hangman(secretWord)
